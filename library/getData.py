49b21fe7436a8d8e42ad203c2040e7044fb77d78
Zack <zack@leanpro.cn>
2018-02-04 17:04:17 +0800

    20180204

--- library/getData.py
+++ library/getData.py
@@ -1,5 +1,7 @@
 
 import csv
+import xlrd
+
 
 def get_csv_data(csv_path):
     """
@@ -18,3 +20,26 @@ def get_csv_data(csv_path):
         rows.append(row)
     
     return rows
+
+
+
+def get_xls_data(xlspath):
+    """
+    从xls文件中读取数据 将数据以列表的形式返回
+
+    @type xls_path:string
+    @param xls_path: xls file path
+    @return list
+    """
+    book = xlrd.open_workbook(xlspath)
+    sh = book.sheet_by_index(0)
+    rows = sh.nrows
+    ncols = sh.ncols
+    alldata=[]
+    for x in range(rows-1):
+        row = []
+        for y in range(ncols):
+            row.append(sh.cell_value(x+1, y))
+        alldata.append(row)
+
+    return alldata
--- scenarios/register.py
+++ scenarios/register.py
@@ -6,7 +6,7 @@ from selenium import webdriver
 from ddt import ddt, data, unpack
 
 
-from library.getData import get_csv_data
+from library.getData import get_xls_data
 from library.getScreenShot import getScreenShotsPath
 
 
@@ -20,19 +20,36 @@ class Register(unittest.TestCase):
         self.driver.implicitly_wait(3)
         self.driver.maximize_window()
 
-    @data(*get_csv_data('./data/scenario-register.csv'))
+    # @data(*get_csv_data('./data/scenario-register.csv'))
+    # @unpack
+    # def test_register(self, username, passwd, repasswd, email, assert_result):
+    #     driver = self.driver
+    #     driver.get('http://118.31.19.120:3000/')
+
+    #     driver.find_element_by_css_selector('a[href="/signup"]').click()
+    #     driver.find_element_by_id('loginname').send_keys(username)
+    #     driver.find_element_by_id('pass').send_keys(passwd)
+    #     driver.find_element_by_id('re_pass').send_keys(repasswd)
+    #     driver.find_element_by_id('email').send_keys(email)
+
+    #     driver.find_element_by_css_selector('.span-primary').click()
+    @data(*get_xls_data('./data/user.xls'))
     @unpack
-    def test_register(self, username, passwd, repasswd, email, assert_result):
-        driver = self.driver
-        driver.get('http://118.31.19.120:3000/')
+    def test_login(self,username,passwd,status,excpetVal):
+        dr = self.driver 
+        dr.get("http://118.31.19.120:3000/")
+        dr.find_element_by_css_selector('body > div.navbar > div > div > ul > li:nth-child(6) > a').click()
+        dr.find_element_by_id('name').send_keys(username)
+        dr.find_element_by_id('pass').send_keys(passwd)
 
-        driver.find_element_by_css_selector('a[href="/signup"]').click()
-        driver.find_element_by_id('loginname').send_keys(username)
-        driver.find_element_by_id('pass').send_keys(passwd)
-        driver.find_element_by_id('re_pass').send_keys(repasswd)
-        driver.find_element_by_id('email').send_keys(email)
+        dr.find_element_by_css_selector(".span-primary").click()
 
-        driver.find_element_by_css_selector('.span-primary').click()
+        if status == "成功":
+            successText = dr.find_element_by_css_selector('#sidebar > div:nth-child(1) > div.inner > div > div > span.user_name > a').text
+            self.assertEqual(successText,excpetVal)
+        else:
+            faildeText = dr.find_element_by_css_selector('#content > div > div.inner > div > strong').text
+            self.assertEqual(faildeText,excpetVal)
 
     def tearDown(self):
         filename = str(time.time()) +".png"
